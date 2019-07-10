3ac25927b070c3341dd8df28de75bb2b0b0b0323
Ha T. Le <51522947+hatmle@users.noreply.github.com>
2019-07-10 11:38:24 -0400

    Update a_variable.py

--- a_variable.py
+++ a_variable.py
@@ -9,5 +9,15 @@ def main():
     datalen="is good"
     print(len(datalen))
     print(datalen[1])
+    
+    # other objects
+    Name="hussein"
+    print(Name,type(Name))
+    
+    Age=22
+    print(Age,type(Age))
+    
+    Slary=12544.5
+    print(Slary,type(Slary))
 
 if __name__ == '__main__':main()
