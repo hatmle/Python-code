2c577f79d9d5367e3560de36d0830df0f3d7a123
Ha T. Le <51522947+hatmle@users.noreply.github.com>
2019-07-12 12:07:56 -0400

    Update a_variable.py

--- a_variable.py
+++ a_variable.py
@@ -19,6 +19,24 @@ def main():
     
     Slary=12544.5
     print(Slary,type(Slary))
+    
+    # dictionary
+    # Student={'Name':"hussein alrubaye",'Age':27,'Slary':232.5};
+    Student=dict(Name="hussein alrubaye",Age=27,Slary=232.5);
+    Student['Name']="Hussein Ahmed"
+    Student["Dept"]="software engineer"
+
+    print(Student,type(Student))
+
+    del Student["Dept"]
+
+    print(Student,type(Student))
+    print(Student['Name'])
+    print(Student['Age'])
+
+    Student.clear()
+
+    print(Student,type(Student))
 
 if __name__ == '__main__':
     main()
