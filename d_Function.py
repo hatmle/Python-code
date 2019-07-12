b64592ebacbd2889abe278b391c2f2f43a694e8c
Ha T. Le <51522947+hatmle@users.noreply.github.com>
2019-07-12 12:06:29 -0400

    Create d_Function.py

---
+++ d_Function.py
@@ -0,0 +1,12 @@
+# sum two numbers
+def SumNumbers(n1,n2):
+    z=n1+n2
+    return z
+
+# main function
+def main():
+    result= SumNumbers(3,5)
+    print("z={}".format(result))
+    
+if __name__ == '__main__':
+    main()
