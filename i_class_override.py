e04577c16a8e8bc6435d5a45e9b03916976b8e12
Ha T. Le <51522947+hatmle@users.noreply.github.com>
2019-07-17 10:20:07 -0400

    Create i_class_override.py

---
+++ i_class_override.py
@@ -0,0 +1,26 @@
+class Operation:
+    def Sum(self,n1,n2):
+        SumResult=n1+n2
+        print("Sum=",SumResult)
+
+    def Sub(self,n1,n2):
+        SubResult=n1-n2
+        print("Sub=",SubResult)
+
+class OperationWithMul(Operation):
+    def Mul(self,n1,n2):
+        MulResult=n1*n2
+        print("Mul=",MulResult)
+
+    def Sub(self,n1,n2):
+        super().Sub(n1,n2)
+
+def main():
+
+    OpMul=OperationWithMul();
+    OpMul.Sub(4,2)
+    OpMul.Sum(10,15)
+    OpMul.Mul(10,2)
+
+if __name__ == '__main__':
+    main()
